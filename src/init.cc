da40d50ad4ff279174128786c63596b5221791b6
Haolin Song <403438485@qq.com>
2017-10-30 10:52:54 +0000

    modify initial varibles sqlunsigned

--- src/init.cc
+++ src/init.cc
@@ -7,6 +7,7 @@ void initDesc(CDescRec *cdr, TESTDATA_TABLE  map){
     cdr->m_ODBCDataType     = map.m_ODBCDataType     ;                        
     cdr->m_SQLDataType      = map.m_SQLDataType      ;                        
     cdr->m_SQLMaxLength     = map.m_SQLMaxLength     ;                         
+    cdr->m_SQLUnsigned      = map.m_DescUnsigned      ;
     cdr->m_DescUnsigned     = map.m_DescUnsigned     ;                         
     cdr->m_SQLCharset       = map.m_SQLCharset       ;                       
     cdr->m_SQLDatetimeCode  = map.m_SQLDatetimeCode  ;                 
